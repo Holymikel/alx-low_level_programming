b7734476140b21d6ec086416a5204410109ae796
Mayowa <mayowajmichael@yahoo.com>
2022-09-10 07:36:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ *main - Use 'puts' to output a string to stdou "Programming is like building a multilingual puzzle" followed by a new line.
+ *
+ *Return: Returns '0' if output string to stdout with no issue
+ *
+ */
 int main(void)
 
 {
