19b2608763115a995acb931b88c8348033b68346
Mayowa <mayowajmichael@yahoo.com>
2022-09-11 08:46:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - this program prints the alphabets in lowercase
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+		printf("\n");
+			return (0);
+}
