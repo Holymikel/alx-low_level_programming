c295fd841d8b1d339074f57ef88844b6890b33db
Mayowa <mayowajmichael@yahoo.com>
2022-09-11 15:02:19 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -18,9 +18,13 @@ int main(void)
 	char mimi;
 
 	for (a = 0 ; a < 10; a++)
+	{
 		putchar(a);
+	}
 	for (mimi = 'a'; mimi <= 'f'; mimi++)
+	{
 		putchar(mimi);
+	}
 	putchar('\n');
 	return (0);
 }
