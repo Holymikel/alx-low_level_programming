705aa3713a47a887ca892f3da284803c521d99ca
Mayowa <mayowajmichael@yahoo.com>
2022-09-11 10:22:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - this program prints all single digit numbers of base 10
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int mimi;
+
+	for (mimi = '0'; mimi < '10'; mimi++)
+	putchar(mimi);
+	printf("\n");
+	return (0);
+}
