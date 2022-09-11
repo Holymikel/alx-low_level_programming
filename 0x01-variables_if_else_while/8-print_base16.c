90657210b05086b6222c8a9f47e3225a469d5d7a
Mayowa <mayowajmichael@yahoo.com>
2022-09-11 14:55:18 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - this program prints the alphabets in lowercase
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int a;
+	char mimi;
+
+	for (a = 0 ; a < 10; a++)
+	{
+		putchar(a)
+	}
+	for (mimi = 'a'; mimi <= 'f'; mimi++)
+	{
+		putchar(mimi);
+	}
+	putchar('\n');
+	return (0);
+}
