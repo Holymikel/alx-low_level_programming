641705a6a62e9eb2e146d0dd6de2b2cb73f21995
Mayowa <mayowajmichael@yahoo.com>
2022-09-11 14:02:55 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - this program prints all the single digits less than 10
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int mimi;
+
+	for (mimi = 0 ; mimi < 10 ; mimi++)
+	putchar(mimi);
+	putchar("\n");
+	return (0);
+}
