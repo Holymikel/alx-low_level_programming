c1ddd62adead929227967c2982980ddc642c0662
Mayowa <mayowajmichael@yahoo.com>
2022-09-12 11:22:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,9 +16,9 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0' ; a <= '9' ; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = a + 1 ; b < 10 ; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
