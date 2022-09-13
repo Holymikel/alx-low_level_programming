85101a968ac6c50fd6a5a0136579a649c6161696
Mayowa <mayowajmichael@yahoo.com>
2022-09-13 05:02:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This program that prints _putchar, followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar(_putchar);
+	printf("\n");
+	return (0);
+}
