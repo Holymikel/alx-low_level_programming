fb1ae6a898e18e44386d6d258087d6c6a8fe754a
Mayowa <mayowajmichael@yahoo.com>
2022-09-22 11:19:28 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,27 @@
+#include <main.h>
 #include <stdio.h>
-#include <string.h>
 /**
  *_strncpy - copies string from source to destination
  *@dest: destination string
  *@src: source string to be copied
- *
+ *@n: largest number of bytes to copy
  *Return: destination string concatenated
  */
 
-char *strncpy(char *dest, const char *src, size_t n)
+char *_strncpy(char *dest, const char *src, int n)
 {
-	size_t i;
+	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while i < n
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
