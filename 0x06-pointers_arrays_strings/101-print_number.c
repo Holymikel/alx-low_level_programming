e704c9b4d27faae8e67cb385d407ad38fe753205
Mayowa <mayowajmichael@yahoo.com>
2022-09-23 13:31:41 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include <main.h>
+#include <stdout.h>
 
 /**
  * print_number - prints an integer
@@ -14,7 +15,7 @@ void print_number(int n)
 		_putchar(45);
 		i = -i;
 	}
-	if (i / 10)
+	if ((i / 10) > 0)
 	{
 		print_number(i / 10);
 	}
